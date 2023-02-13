b10a764ffea7233486cbb51827b041f567a2301b
Alfred Broderick <AlfredBr@outlook.com>
2023-02-13 15:29:31 -0500

    Update ConsoleBox.cs

--- Source/menu/ConsoleBox.cs
+++ Source/menu/ConsoleBox.cs
@@ -1,43 +1,64 @@
 ﻿namespace menu;
 
-public static class ConsoleBox
+public class ConsoleBox
 {
-    public static ConsoleColor OutlineColor { get; set; } = ConsoleColor.DarkYellow;
-    public static void WriteLine(params string[] contents)
+	static ConsoleBox()
+	{
+		consoleBox = new ConsoleBox();
+	}
+	public static ConsoleBox SetOutlineColor(ConsoleColor color)
+	{
+		consoleBox.outlineColor = color;
+		return consoleBox;
+	}
+	public static void WriteLine(params string[] contents)
+	{
+		consoleBox.WriteLineInternal(contents);
+	}
+	private static ConsoleBox consoleBox;
+    internal ConsoleColor outlineColor = ConsoleColor.DarkYellow;
+	internal void WriteLineInternal(params string[] contents)
+	{
+		int[] lengths = contents.Select(t => t.Length).ToArray();
+
+		Console.WriteLine();
+		Console.ForegroundColor = consoleBox.outlineColor;
+		Console.Write("┌");
+		for (int l = 0; l < lengths.Length - 1; l++)
+		{
+			var length = lengths[l];
+			Console.Write("─".PadRight(length + 2, '─'));
+			Console.Write("┬");
+		}
+		Console.Write("─".PadRight(lengths.Last() + 2, '─'));
+		Console.WriteLine("┐");
+
+		foreach (var content in contents)
+		{
+			Console.Write("│ ");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.Write(content);
+			Console.ForegroundColor = consoleBox.outlineColor;
+			Console.Write(" ");
+		}
+		Console.WriteLine("│");
+
+		Console.Write("└");
+		for (int l = 0; l < lengths.Length - 1; l++)
+		{
+			var length = lengths[l];
+			Console.Write("─".PadRight(length + 2, '─'));
+			Console.Write("┴");
+		}
+		Console.Write("─".PadRight(lengths.Last() + 2, '─'));
+		Console.WriteLine("┘");
+		Console.ForegroundColor = ConsoleColor.White;
+	}
+}
+public static class ConsoleBoxExtensions
+{
+    public static void WriteLine(this ConsoleBox consoleBox, params string[] contents)
     {
-        var lengths = contents.Select(t => t.Length).ToArray();
-
-        Console.WriteLine();
-        Console.ForegroundColor = OutlineColor;
-        Console.Write("┌");
-        for (int l = 0; l < lengths.Length - 1; l++)
-        {
-            var length = lengths[l];
-            Console.Write("─".PadRight(length + 2, '─'));
-            Console.Write("┬");
-        }
-        Console.Write("─".PadRight(lengths.Last() + 2, '─'));
-        Console.WriteLine("┐");
-
-        foreach (var content in contents)
-        {
-            Console.Write("│ ");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write(content);
-            Console.ForegroundColor = OutlineColor;
-            Console.Write(" ");
-        }
-        Console.WriteLine("│");
-
-        Console.Write("└");
-        for (int l = 0; l < lengths.Length - 1; l++)
-        {
-            var length = lengths[l];
-            Console.Write("─".PadRight(length + 2, '─'));
-            Console.Write("┴");
-        }
-        Console.Write("─".PadRight(lengths.Last() + 2, '─'));
-        Console.WriteLine("┘");
-        Console.ForegroundColor = ConsoleColor.White;
-    }
+		consoleBox.WriteLineInternal(contents);
+	}
 }
